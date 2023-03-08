95790b2237bb728ba1b8b0d00b171249345892a7
Rabih-nader <rabih.nadir@gmail.com>
2023-03-08 18:34:12 -0500

    SignUP and navbar

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,18 @@
-// SignUp.js
-
 import firebase from "../firebase";
 import { useState } from "react";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [photo, setPhoto] = useState(null);
+  const [photoUrl, setPhotoUrl] = useState("");
 
-  const handleSignUp = () => {
+  const handleSignUp = (e) => {
+    e.preventDefault();
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
@@ -15,6 +20,14 @@ const SignUp = () => {
         // Signed up successfully
         const user = userCredential.user;
         console.log(user);
+        // Update user profile with additional information
+        user.updateProfile({
+          phoneNumber,
+          address,
+          dob,
+          gender,
+          photoUrl,
+        });
       })
       .catch((error) => {
         // An error occurred
@@ -24,10 +37,25 @@ const SignUp = () => {
       });
   };
 
+  const handlePhotoUpload = (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebase.storage().ref(`users/${email}/${file.name}`);
+    storageRef.put(file).then((snapshot) => {
+      console.log("Uploaded a file");
+      snapshot.ref.getDownloadURL().then((url) => {
+        setPhotoUrl(url);
+      });
+    });
+  };
+
+  const handlePhotoPickerClick = () => {
+    document.getElementById("photoInput").click();
+  };
+
   return (
     <div>
       <h2>Sign Up</h2>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={handleSignUp}>
         <label htmlFor="email">Email</label>
         <input
           type="email"
@@ -44,12 +72,58 @@ const SignUp = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button type="submit" onClick={handleSignUp}>
-          Sign Up
-        </button>
-      </form>
-    </div>
-  );
-};
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input
+          type="text"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
 
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
+        <label htmlFor="dob">Date of Birth</label>
+        <input
+          type="date"
+          id="dob"
+          value={dob}
+          onChange={(e) => setDob(e.target.value)}
+        />
+
+        <label htmlFor="gender">Gender</label>
+        <select id="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="">Select gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+
+        <label htmlFor="photo">Profile Photo</label>
+        <input
+          type="file"
+          id="photoInput"
+          accept="image/*"
+          onChange={handlePhotoUpload}
+          style={{ display: "none" }}
+        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+{photoUrl ? (
+<img src={photoUrl} alt="Profile" style={{ width: 100, height: 100 }} />
+) : (
+<button type="button" onClick={handlePhotoPickerClick}>
+Choose Photo
+</button>
+)}
+</div>
+<button type="submit">Sign Up</button>
+  </form>
+</div>
+);
+};
 export default SignUp;
